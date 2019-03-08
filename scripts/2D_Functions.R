94e188f9e16d36926e9aad6c6b4b314c4a15d59c
sjcruz <scruz@bren.ucsb.edu>
2019-03-08 10:32:05 -0800

    Re-ran everything

--- scripts/2D_Functions.R
+++ scripts/2D_Functions.R
@@ -1,22 +1,142 @@
 ######################################################################
+#Midriff's Watch Project Functions 
+#December 10, 2018
+######################################################################
+
+
+######################################################################
+#CMSY2 Function
+#
+######################################################################
+
+Species_CMSY2<-function(data, priors){
+  library(dplyr)
+  require(datalimited2)
+  
+  variables <- data.frame(Name=NA,
+                          Adjusted=NA,
+                          r= NA, r.low = NA, r.hi = NA,
+                          k= NA, k.low = NA,k.hi = NA,
+                          msy= NA,msy.low = NA, msy.hi = NA,
+                          bmsy= NA, bmsy.low = NA, bmsy.hi = NA,
+                          year=NA,	
+                          catch=NA,	
+                          catch_ma=NA,	
+                          b=NA,	
+                          b_lo=NA,	
+                          b_hi=NA,	
+                          bbmsy=NA,	
+                          bbmsy_lo=NA,	
+                          bbmsy_hi=NA,	
+                          s=NA,	
+                          s_lo=NA,	
+                          s_hi=NA,	
+                          f=NA,	
+                          f_lo=NA,	
+                          f_hi=NA,	
+                          fmsy=NA,	
+                          fmsy_lo=NA,	
+                          fmsy_hi=NA,	
+                          ffmsy=NA,	
+                          ffmsy_lo=NA,	
+                          ffmsy_hi=NA,	
+                          er=NA)
+  
+  ref_ts <- data.frame(Name=NA,
+                       Adjusted=NA,
+                       year=NA,	
+                       catch=NA,	
+                       catch_ma=NA,	
+                       b=NA,	
+                       b_lo=NA,	
+                       b_hi=NA,	
+                       bbmsy=NA,	
+                       bbmsy_lo=NA,	
+                       bbmsy_hi=NA,	
+                       s=NA,	
+                       s_lo=NA,	
+                       s_hi=NA,	
+                       f=NA,	
+                       f_lo=NA,	
+                       f_hi=NA,	
+                       fmsy=NA,	
+                       fmsy_lo=NA,	
+                       fmsy_hi=NA,	
+                       ffmsy=NA,	
+                       ffmsy_lo=NA,	
+                       ffmsy_hi=NA,	
+                       er=NA)
+  
+  Genus <- as.vector(unique(data$Genus))
+  
+  for (s in Genus){
+    df <- data%>% filter(Genus == s) 
+    year<-as.vector(df$Ano, mode='numeric')
+    catches<-(df[,4:7])
+    var <-subset(priors, Genus == s)
+    resilience <- var$Resilience
+    r.low<- var$r_lo
+    r.hi<- var$r_hi
+    stb.low<- var$stb.low
+    stb.hi<- var$stb.hi
+    endb.low<- var$endb.low
+    endb.hi<- var$endb.hi
+    
+    for (i in 1:ncol(catches)){
+      catch <- catches[,i]
+      x_last<- nrow(catch)
+      adjusted <- colnames(catches[i])
+      
+      cmsy <- datalimited2::cmsy2(year=year, catch=catch, resilience = resilience, 
+                                  r.low=r.low, r.hi=r.hi,
+                                  stb.low =stb.low, stb.hi=stb.hi,
+                                  endb.low=endb.low, endb.hi=endb.hi)
+      output<- cmsy$ref_ts
+      
+      output<- output%>%
+        mutate(Name = s, 
+               Adjusted = c(adjusted))
+      Name <- s
+      Adjusted <- adjusted
+      R <- cmsy$ref_pts[1,2:4]
+      K <- cmsy$ref_pts[2,2:4]
+      MSY <- cmsy$ref_pts[3,2:4]
+      BMSY <- cmsy$ref_pts[5,2:4]
+      tail <- cmsy$ref_ts[tail(nrow(cmsy$ref_pts)),]
+      out <- cbind(Name, Adjusted, R, K, MSY, BMSY, tail)
+      names(out)[1:14] <- c("Name", "Adjusted",
+                            "r", "r.low", "r.hi", 
+                            "k", "k.low", "k.hi",
+                            "msy", "msy.low", "msy.hi",
+                            "bmsy", "bmsy.low", "bmsy.hi")
+      
+      variables <- rbind(variables, out)
+      
+      ref_ts<- rbind(ref_ts, output)
+    }
+  }
+  return (list(variables[-1,], ref_ts[-1,]))
+}
+
+
+######################################################################
 #Open Access Fishing Effort dynamic over time 
 #Distribution of Fishing effort (profit and cost)
 ######################################################################
 
 fishing.effort.OA<- function (p, MSY, r, bmsy, fmsy, F.mat, B.mat, c, profit.msy, t){
-  #F.mat[,] <- fmsy
   f.mat <- F.mat/fmsy 
   b.mat <- B.mat/bmsy
   MSY <- MSY/11236 
   c <- c/11236 
   profit.msy <- profit.msy/11236 
   
-  revenue <- p* F.mat * B.mat
+  revenue <- p * F.mat * B.mat
   profit <-  revenue - (c * fmsy * f.mat)
   
-  PV <- profit/((1 + 0.05)^t)
+  PV <- profit/((1 + 0.1)^t) #10% discount rate
   
-  f.mat <- f.mat + (0.1 * (profit/profit.msy))
+  f.mat <- f.mat + (0.1 * (profit/profit.msy)) #lambda = 0.1
   F.mat <- f.mat * fmsy
   
   return(list(F.mat, profit, PV, revenue))}
@@ -28,7 +148,7 @@ fishing.effort.OA<- function (p, MSY, r, bmsy, fmsy, F.mat, B.mat, c, profit.msy
 
 MPA.Model <- function(r, K, Fishing, B, years, MPA.mat, mrate, MSY, bmsy, fmsy, p, c, profit.msy, start.year){
   patches <- 106
-  F.mat <- matrix(ncol=patches, nrow=patches, Fishing) #Change back to Fishing
+  F.mat <- matrix(ncol=patches, nrow=patches, Fishing) 
   K.mat <- matrix(ncol=patches, nrow=patches, K/11236)  
   B.mat <- matrix(ncol=patches, nrow=patches, B/11236)  
   arriving <- matrix(nrow=nrow(MPA.mat), ncol= ncol(MPA.mat), 0) 
@@ -113,7 +233,7 @@ MPA.Model <- function(r, K, Fishing, B, years, MPA.mat, mrate, MSY, bmsy, fmsy, 
 ######################################################################
 
 Scenarios <- function(data, years, MPA.mat, start.year) {
-  out<-data.frame(Name=NA,Adjusted=NA)
+  out<-data.frame(Name=NA, Adjusted=NA)
 
   growth <- as.numeric(data[,c("r", "r.low", "r.hi")])
   car.cap <- as.numeric(data[,c("k", "k.low", "k.hi")])
@@ -245,6 +365,6 @@ Biological.Model<- function(df, years, MPA.mat, start.year) {
     results <- rbind(results, scen)
   }
   results<-results%>%
-    mutate(Status = start.year)
+    mutate(Implementation_year = start.year)
   return(results)
 }
